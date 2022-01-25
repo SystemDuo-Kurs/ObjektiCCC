39a6f61b607587ef62f506b216b4527146eb2764
Dragan Pepic <nedimco2006@gmail.com>
2022-01-25 17:59:03 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    public static void Main()
+    {
+        List<Osoba> osobe = new();
+
+        do
+        {
+            Osoba o = new();
+            Console.Write("Unesite ime: ");
+            o._name = Console.ReadLine();
+            Console.Write("Unesite prezime: ");
+            o._surname = Console.ReadLine();
+
+            osobe.Add(o);
+
+            foreach (Osoba neko in osobe)
+            {
+                Console.WriteLine($"{neko._name} {neko._surname}");
+            }
+        } while (true);
+    }
+}
+
+internal class Osoba
+{
+    public string _name;
+    public string _surname;
+
+    public void JaviSe()
+    {
+        Console.WriteLine($"Hej, ja sam {_name} :D");
+    }
+}
